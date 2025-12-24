c2b88dec0634811d1fcc34f31517699a0043c502
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2025-12-25 03:01:11 +0530

    Variables & Declaration 50% Done

---
+++ 01-JavaScript/02-Variables-&-Declaration/script.js
@@ -0,0 +1,34 @@
+// -- var, let, const — line-by-line comparison
+
+// // a = 12; Ganda trika to create variables
+// var a;
+// var a = 12;
+// let a;
+// let a = 12;
+// // const a; Error
+// const a = 12;
+
+// // Declarations & Initialization
+// var a; // Declare Karna
+// var a = 12; // Declare & Initialize
+
+// var a = "Abhishek";
+// // -- Window me add hota hai
+// // -- function scoped hota hai
+// // -- aap firse declare kar sakte ho same name se & error nhi ayega.
+
+// var a = 12;
+// var a = 13; // No error
+
+// // let a = 12;
+// // let a = 13; // Error
+
+// const discount = 12;
+// const dulha = "Lab";
+// const dulhan = "Laby";
+
+// var 
+// -- Window me add hota hai
+// -- functional scoped hota hai
+// -- can be redeclared
+
