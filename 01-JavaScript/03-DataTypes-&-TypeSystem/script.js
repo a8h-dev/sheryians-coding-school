a2d91a2b00d3707486e39de28d9608c9819ed2b2
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2025-12-25 15:37:07 +0530

    Primitive & Reference Difference Done

---
+++ 01-JavaScript/03-DataTypes-&-TypeSystem/script.js
@@ -0,0 +1,25 @@
+// data types:
+// data ka type
+
+// 12 "Abhishek" true [] {} () null undefined NaN Symbol bigInt 
+
+// primitive -> Aise sari values jinko copy krne par tumhe ek real copy mil jaye 
+// -- string, number, boolean, null, undefined, symbol, bigInt
+
+// reference -> Inko copy krne par aapko real copy nhi milegi par aapko reference milega parent ka
+// -- arrays [], objects {}, functions ()
+
+// male | female | others -- Data Types aise hi hai
+// primitive | reference -- Data types inhi do me se kisi ek me fall krenge
+
+
+// let a = 12; // number // primitive datatype
+// let b = a;
+// a = b + 3;
+// console.log(a);
+
+// let a = [1,2,3]; // array // reference datatype
+// let b = a;
+// b.pop();
+// console.log(a);
+// console.log(b);
