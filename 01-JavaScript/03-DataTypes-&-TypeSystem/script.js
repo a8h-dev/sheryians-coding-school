eed1441d5f853e466e6df705543d5452f7ce8bda
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2025-12-25 16:00:33 +0530

    Primitive DataTypes Done

--- 01-JavaScript/03-DataTypes-&-TypeSystem/script.js
+++ 01-JavaScript/03-DataTypes-&-TypeSystem/script.js
@@ -23,3 +23,60 @@
 // b.pop();
 // console.log(a);
 // console.log(b);
+
+
+// -- string, number, boolean, null, undefined, symbol, bigInt
+// -- arrays [], objects {}, functions ()
+
+// string
+// '' - single quotes
+// "" - double quotes
+// `` - backticks
+
+// number
+// 12 - integer
+// 12.3 - number
+
+// boolean
+// let a = true;
+// let b = false;
+
+// null
+// null ka matlab hai aapne jaan bujhkr koi value nhi di.
+// let selectedStudents = null; // mtlb koi student ko select nhi kiya hai
+
+// undefined
+// undefined ka matlab apne ek variable banaya aur usko value nhi di to jo value by default mili wo hai undefined 
+// let a;
+
+
+
+// symbol
+// unique immutable value
+// future me ham koi libraries use krenge ab is case me un libraries me kai baar kuch fields hoti hai jinse similar ham bhi bana dete hain aur galti se hamari banai hui fields us library ki original fields ko change kar deta hai
+
+// let sheryjs = {
+//     uid: 12,
+//     model: "Abhishek"
+// }
+// sheryjs.uid = 1;
+
+// let u1 = Symbol("uid");
+// let u2 = Symbol("uid");
+// u1 === u2 -> false
+
+// let obj = {
+//     uid: 1,
+//     name: "Abhishek",
+//     age: 12,
+//     email: "test@gmail.com"
+// };
+// let u1 = Symbol("uid");
+// obj[u1] = "001";
+
+
+// bigInt
+// Number.MAX_SAFE_INTEGER
+// let a = 9007199254740991n;
+// a = a + 32n
+// console.log(a);
