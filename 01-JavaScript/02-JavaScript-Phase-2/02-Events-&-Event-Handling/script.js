b9da47b66940b6279f7996e15cfc100fdf4399aa
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-02 15:16:13 +0530

    MINI PROJECT 3 -> Dynamic Data to Card

--- 01-JavaScript/02-JavaScript-Phase-2/02-Events-&-Event-Handling/script.js
+++ 01-JavaScript/02-JavaScript-Phase-2/02-Events-&-Event-Handling/script.js
@@ -94,16 +94,61 @@
             
 // **MINI PROJECT 2 -> CUSTOM FILE UPLOAD BUTTON**
 
-let btn = document.querySelector("#btn");
-let fileInp = document.querySelector("#fileInp");
+// let btn = document.querySelector("#btn");
+// let fileInp = document.querySelector("#fileInp");
 
-btn.addEventListener("click", function(){
-    fileInp.click();
-})
-fileInp.addEventListener("change", function(dets){
-    const file = dets.target.files[0];
-    if(file){
-        btn.textContent = file.name;
-    }
-})
+// btn.addEventListener("click", function(){
+//     fileInp.click();
+// })
+// fileInp.addEventListener("change", function(dets){
+//     const file = dets.target.files[0];
+//     if(file){
+//         btn.textContent = file.name;
+//     }
+// })
 
+// **SUBMIT EVENT**
+// ==MINI PROJECT 2 -> Dynamic Data to Card==
+let form = document.querySelector("form");
+let input = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+
+form.addEventListener("submit", function(dets){
+    dets.preventDefault(); // Reload/Refresh na kre
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
+
+    let img = document.createElement("img");
+    img.setAttribute(
+        "src",
+        input[0].value
+    );
+
+    let h3 = document.createElement("h3");
+    h3.textContent = input[1].value;
+
+    let h5 = document.createElement("h5");
+    h5.textContent = input[2].value;
+
+    let p = document.createElement("p");
+    p.textContent = input[3].value;
+
+
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);  
+
+    main.appendChild(card);
+
+    input.forEach(function(inp){
+        if(inp.type !== "submit"){
+            inp.value = "";
+        }
+    })
+})
