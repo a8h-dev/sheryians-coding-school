ae71e9dd96b4295a9b37df176e09f7c953d166da
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-07 12:31:45 +0530

    ES6 Classes: constructor, methods, extends, super Done

--- 01-JavaScript/03-JavaScript-Phase-3/03-Object-Oriented JavaScript/script.js
+++ 01-JavaScript/03-JavaScript-Phase-3/03-Object-Oriented JavaScript/script.js
@@ -155,3 +155,118 @@
 // let pencil1 = new CreatePencil("Nataraj", 10, "black", "Nataraj");
 // let pencil2 = new CreatePencil("Doms", 10, "red", "Doms");
 
+
+
+// ------------- ES6 Classes: constructor, methods, extends, super
+// **class kya hai?**
+// wahi constructor function jo ham abhi bana rhe the usi ko thoda saja kar likhne ka trika hai
+// log confuse hote hain but easy
+
+// Khayal rakhne wali baat -> yeha pe function wale bracket () nhi lagte sidhe curly braces lagte hain
+
+// iske andar pehli chiz jo ham banate hain wo hota hai constructor
+// constructor kya hota hai?
+// constructor pehli chiz hoti hai jo chalta hai
+// jaise ham likhte hain new CreatePencil() toh isse constructor chal rha hota hai
+
+// **Kaam kya hota hai iska??**
+// default values set krna ya initialize krna, variables create krna ye sab iske hote hain
+// ye sab ham sirf kaam krke hi sikh payenge
+
+class CreatePencil{
+    // **constructor**
+    constructor(name, company, price, color){
+        this.name = name;
+        this.company = company;
+        this.price = price;
+        this.color = color;
+    }
+    // toh constructor ke andar aap apne variables sare define kr dete ho
+    // aur uske bahar aap apne methods wagera bana sakte ho
+    // **methods**
+    write(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = text;
+        h1.style.color = this.color;
+        document.body.appendChild(h1);
+    }
+    // yeha arrow function use hua
+    erase(){
+        document.body.querySelectorAll("h1").forEach((elem) => {
+            if(elem.style.color === this.color){
+                elem.remove();
+            }
+        })
+    }
+}
+
+let p1 = new CreatePencil("nataraj", "nataraj", 10, "black");
+let p2 = new CreatePencil("apsara", "apsara", 15, "blue");
+
+// Mindset behind it
+// now you've seen how Object-Oriented works
+// kitna easy ho gya sabkuch this is why Object Oriented is one of best approach to do things
+// ab aapne wo sab nhi likha koi bhi faltu giri nhi likhi hai ki boht hi random ajeeb sa spaghetti code nhi likha ki kahi se bhi kuch bhi pick kro aur hatao na na na...
+// Cohort me Object Oriented ka use krke pura OS banaya hai. 
+
+// **extends**
+// ye kya hota hai??
+// basically already ek class aap banake rakhe hue ho, aapne nayi class banayi aur nayi class pichli class ki sari values ko use kr sakti hai aur apni khudki kuch hold krti hai.
+
+// Example: 
+class User{
+    // **constructor**
+    constructor(name, address, username, email){
+        this.name = name;
+        this.address = address;
+        this.username = username;
+        this.email = email;
+        this.role = "user";
+    }
+
+    // **methods**
+    checkRole(){
+        return `You are a ${this.role}`;
+    }
+    write(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = `${this.name} : ${text}`;
+        document.body.appendChild(h1);
+    }
+}
+
+// ab maan lete hain aapke pass sirf users nhi aapke pass Admins bhi hain
+// yeha pe role suru hota hai
+// kya aap mante ho ki har Admin ke pass wo sab hoga jo user ke pass hai? name, address, username, email, role?? -- haan 
+// toh ham ye duplicate krke wapis se nhi likhna chahenge
+// issiliye ham likhenge extends kaise?? dekho niche
+// **extends**
+class Admin extends User{
+    // ye kya hai??
+    // basically ham ye keh rhe hain ki Admin class wo sab rakhti hai jo User ke pass hai.
+
+    // super ko call krne ke baad aapko uper wale values ko constructor me bhejna prega
+    // fir super me sari chizen aapko pass krni pregi
+    // Admin ka apna constructor
+    constructor(name, address, username, email){
+        // aapko pehle super() ko call krna hoga mtlb -> parent ke constructor ko call krna prega
+        // super pe pass krne ka mtlb uper User wale constructor ke pass chala jayega
+        // **super**
+        super(name, address, username, email);
+
+        // ab admin ki khudki chizen aapko rakhni hai
+        this.role = "admin";
+    }
+    // ab yeha pe ek new method bana rhe hain remove;
+    // iski khas baat ye hui ki sirf Admin remove kr sakta hai User remove nhi kar payega.
+    remove(){
+        document.querySelectorAll("h1").forEach(function(elem){
+            elem.remove();
+        })
+    }
+
+}
+
+let u1 = new User("Abhishek", "Bhopal", "a8h-dev", "a8h.dev@mail.com");
+let u2 = new User("Sakshi", "Bhopal", "sakshi123", "sakshi123@mail.com");
+let a1 = new Admin("admin1", "India", "adminnnnn1111", "admin1@gmail.com");
