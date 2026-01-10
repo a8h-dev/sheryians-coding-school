6e23856ff180f4d4cffe2560a7151d2de1f9e645
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-10 09:42:35 +0530

    01 - Design Patterns Done, Topic -> Observer Pattern (basic pub-sub) Done

--- 01-JavaScript/04-JavaScript-Phase-4/01-Design-Patterns-in-JavaScript/script.js
+++ 01-JavaScript/04-JavaScript-Phase-4/01-Design-Patterns-in-JavaScript/script.js
@@ -126,3 +126,48 @@
 // let iphone = createProduct("iphone", 70000);
 // let kitkat = createProduct("kitkat", 10);
 // iphone.buy(6);
+
+
+
+
+// --------------------------------------------------------------
+
+// --------------------- Observer Pattern (basic pub-sub)
+
+class YoutubeChannel{
+    constructor(){
+        this.subscribers = [];
+    }
+    subscribe(user){
+        this.subscribers.push(user);
+        user.update(`${user.name}, You have subscribed the channel.`);
+    }
+    unsubscribe(user){
+        this.subscribers = this.subscribers.filter((sub) => sub !== user);
+        user.update(` ${user.name}, You have un-subscribed the channel.`);
+    }
+    notify(message){
+        this.subscribers.forEach((sub) => sub.update(message));
+    }
+}
+
+class User{
+    constructor(name){
+        this.name = name;
+    }
+    update(data){
+        console.log(`${this.name}, ${data}.`);
+    }
+}
+
+let sheryians = new YoutubeChannel();
+let user1 = new User("Abhishek");
+let user2 = new User("Amit");
+
+sheryians.subscribe(user1);
+sheryians.subscribe(user2);
+
+sheryians.notify("new video is live on the channel...");
+sheryians.notify("we are closing the channel, because it's april 1st");
+
+// RESEARCH TIP: jao chatGPT se baate kro usse, new chizen create kro, aur chizen sikho.
