56ada6538877e6345a4857e8031b95d259f3d62c
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-29 22:45:50 +0530

    04 Setting up react with vite Done

---
+++ 02-React-JS/04-Setting-Up-React-with-Vite/01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+// rafce
+// <>Fragments</>
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Abhishek Mahto</h1>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ 02-React-JS/04-Setting-Up-React-with-Vite/notes.md
@@ -0,0 +1,5 @@
+- Old command for creating react folder -> npm create-react-app
+- but developers don't use this method (outdated)
+
+- correct method (use vite)
+- npm create vite
