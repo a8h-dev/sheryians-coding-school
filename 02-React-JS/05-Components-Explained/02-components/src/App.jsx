9bae4e227f73c919e76d3583ec82632dd4f52107
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-01-29 23:09:19 +0530

    Learning Components in React JS

---
+++ 02-React-JS/05-Components-Explained/02-components/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Abhishek Mahto</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, perspiciatis?</p>
+      </div>
+    </div>
+  );
+}
+export default App;
