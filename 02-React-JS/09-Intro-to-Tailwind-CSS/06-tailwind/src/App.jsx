3e7b86614092e5607a860c595a5db86dc6e8c3ba
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-03 02:04:01 +0530

    09 Intro to tailwind css Done

---
+++ 02-React-JS/09-Intro-to-Tailwind-CSS/06-tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 p-2.5'>
+      <h1 className='text-amber-600 text-2xl'>Holllaaaa!!</h1>
+    </div>
+  );
+}
+
+export default App;
