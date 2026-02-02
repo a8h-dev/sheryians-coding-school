1417905afc9d5fcd75a48aab791c660a0ae51f2c
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-03 00:10:01 +0530

    Job opening card proect using react props Done

--- 02-React-JS/07-Mini-Projects-using-Props/04-cards-project/src/App.jsx
+++ 02-React-JS/07-Mini-Projects-using-Props/04-cards-project/src/App.jsx
@@ -1,37 +1,120 @@
-import React from 'react';
-import {Bookmark} from 'lucide-react';
+import React from "react";
+import Card from "./components/Card";
 
 const App = () => {
+  
+  const jobOpenings = [
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/500px-Google_%22G%22_logo.svg.png",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/1200/external-amazon-an-american-multinational-technology-company-that-focuses-on-e-commerce-logo-color-tal-revivo.jpg",
+    companyName: "Amazon",
+    datePosted: "2 weeks ago",
+    post: "Software Development Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$55/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://logos-world.net/wp-content/uploads/2021/11/Meta-Emblem.png",
+    companyName: "Meta",
+    datePosted: "1 week ago",
+    post: "React Developer",
+    tag1: "Remote",
+    tag2: "Senior Level",
+    pay: "$70/hr",
+    location: "Remote, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCvh-j7HsTHJ8ZckknAoiZMx9VcFmsFkv72g&s",
+    companyName: "Apple",
+    datePosted: "10 days ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$75/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://platform.theverge.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/15844974/netflixlogo.0.0.1466448626.png?quality=90&strip=all&crop=1.2535702951444%2C0%2C97.492859409711%2C100&w=2400",
+    companyName: "Netflix",
+    datePosted: "3 weeks ago",
+    post: "UI Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$80/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDiQXGMUd-boRykgZmJXW-MG1JD2x8GHwIyw&s",
+    companyName: "Microsoft",
+    datePosted: "4 days ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$50/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://pngimg.com/d/tesla_logo_PNG12.png",
+    companyName: "Tesla",
+    datePosted: "2 months ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$85/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-1956.jpg?width=672&height=454&name=ibm-logo-1956.jpg",
+    companyName: "IBM",
+    datePosted: "6 days ago",
+    post: "Cloud Engineer",
+    tag1: "Part Time",
+    tag2: "Mid Level",
+    pay: "$40/hr",
+    location: "Kolkata, India"
+  },
+  {
+    brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5eRETWKNQ0IuSETVDffHht0BpBthJtTH9Fg&s",
+    companyName: "Oracle",
+    datePosted: "10 weeks ago",
+    post: "Java Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$60/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://i.pinimg.com/736x/56/3a/a2/563aa2189ef92dc242a7db5b91078804.jpg",
+    companyName: "Adobe",
+    datePosted: "8 days ago",
+    post: "Product Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$65/hr",
+    location: "Noida, India"
+  }
+];
+
   return (
-    <div className='parent'>
-
-      <div className="card">
-        <div className="top">
-          <img src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/1200/external-amazon-an-american-multinational-technology-company-that-focuses-on-e-commerce-logo-color-tal-revivo.jpg" alt="logo" />
-          <button>Save <Bookmark /></button>
+    <div className="parent">
+      {jobOpenings.map(function(elem, idx){
+        return <div key={idx}>
+          <Card logo={elem.brandLogo} company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} tag2={elem.tag2} pay={elem.pay} location={elem.location} />
         </div>
-
-        <div className="center">
-            <h3>Amazon <span>5 days ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
-            <div>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-        </div>
-
-        <div className="bottom">
-            <div>
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai, India</p>
-              </div>
-            </div>
-        </div>
-      </div>
-
+      })}
     </div>
   );
-}
+};
 
 export default App;
