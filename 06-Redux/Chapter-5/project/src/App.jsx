66de17d41f87764023aea95f74ac70818a0b30a5
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-11 21:45:45 +0530

    Unsplash photo api fetch created

--- 06-Redux/Chapter-5/project/src/App.jsx
+++ 06-Redux/Chapter-5/project/src/App.jsx
@@ -1,9 +1,12 @@
-
+import { fetchPhotos } from "./api/mediaApi";
 
 const App = () => {
   return (
     <div className="h-screen text-white w-full bg-gray-950">
-      
+      <button onClick={ async () => {
+        const data = await fetchPhotos('dog');
+        console.log(data.results);
+      }}>Get Photos</button>
     </div>
   );
 }
