21812e14e5f23e48599604c17a0526449da4b624
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-11 19:01:30 +0530

    Let's Create the Project

---
+++ 06-Redux/Chapter-4/project-intro.md
@@ -0,0 +1,5 @@
+vite + react
+tailwind css
+axios
+redux
+react router dom
---
+++ 06-Redux/Chapter-5/project/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="h-screen text-white w-full bg-gray-950">
+      
+    </div>
+  );
+}
+
+export default App;
