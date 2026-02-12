30d1f04d9771e1b02692d9c79dbabc933da85258
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-02-12 15:36:54 +0530

    Result Grid Done let's show data

--- 06-Redux/Chapter-5/project/src/components/ResultGrid.jsx
+++ 06-Redux/Chapter-5/project/src/components/ResultGrid.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPhotos, fetchVideos } from "../api/mediaApi";
 import {
-  setQuery,
   setLoading,
   setError,
   setResults,
@@ -9,43 +8,57 @@ import {
 import { useEffect } from "react";
 
 const ResultGrid = () => {
+  const dispatch = useDispatch();
+
   const { query, activeTab, results, loading, error } = useSelector(
     (store) => store.search,
   );
 
   useEffect(
     function () {
+        if (!query) return;
       const getData = async () => {
-        let data;
-        if (activeTab == "photos") {
-          let response = await fetchPhotos(query);
-          data = response.results.map((item) => ({
-            id: item.id,
-            type: 'photo',
-            title: item.alt_description,
-            thumbnail: item.urls.small,
-            src: item.urls.full
-          }))
+        try {
+          dispatch(setLoading());
+          let data = [];
+          if (activeTab == "photos") {
+            let response = await fetchPhotos(query);
+            data = response.results.map((item) => ({
+              id: item.id,
+              type: "photo",
+              title: item.alt_description,
+              thumbnail: item.urls.small,
+              src: item.urls.full,
+            }));
+          }
+          if (activeTab == "videos") {
+            let response = await fetchVideos(query);
+            data = response.videos.map((item) => ({
+              id: item.id,
+              type: "video",
+              title: item.user.name || "video",
+              thumbnail: item.image,
+              src: item.video_files[0].link,
+            }));
+          }
+          dispatch(setResults(data));
+        } catch (error) {
+          dispatch(setError(err.message));
         }
-        if (activeTab == "videos") {
-          let response = await fetchVideos(query);
-          data = response.videos.map((item) => ({
-            id: item.id,
-            type: 'video',
-            title: item.user.name || 'video',
-            thumbnail: item.image,
-            src: item.video_files[0].link
-          }))
-        }
-        console.log(data);
       };
       getData();
     },
     [query, activeTab],
   );
 
+  if (error) return <h1>Error</h1>;
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div>
+      {results.map((item, idx) => {
+        return <div key={idx}>{item.title}</div>;
+      })}
     </div>
   );
 };
